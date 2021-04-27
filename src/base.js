2da2709d2fd41c0e194b6ccff4fa94bef046ec25
Shivam <aryansn0101@gmail.comgit config --global user.name Your>
2021-04-27 08:07:05 +0530

    commit

---
+++ src/base.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+ apiKey: "AIzaSyBk57efZHr2QHH8yI1oriHBFPf2Z2kGY0I",
+  authDomain: "adsghar-a94ca.firebaseapp.com",
+  projectId: "adsghar-a94ca",
+  storageBucket: "adsghar-a94ca.appspot.com",
+  messagingSenderId: "166707724075",
+  appId: "1:166707724075:web:b1b885bf26bdd144064f6e",
+  measurementId: "G-V6RRZ2LRTF"
+});
+
+export default app;
